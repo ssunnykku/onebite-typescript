1188440d3e3c3bb7f7b4ee8cf26f8a659b127113
ssunnykku <ssunnykku@gmail.com>
2023-06-23 13:35:29 +0900

    섹션3_타입 좁히기

---
+++ section3/src/chapter7.ts
@@ -0,0 +1,33 @@
+/**
+ * 타입 좁히기
+ * 조건문 드을 이용해 넓은타입에서 좁은 타입으로
+ * 타입을 상황에 따라 좁히는 방법을 이야기한다.
+ */
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+// value => number : toFixed
+// value => string : toUpperCase
+// value => Date : getTime
+// value => Person : name은 age살 입니다.
+function func(value: number | string | Date | null | Person) {
+  if (typeof value === "number") {
+    console.log(value.toFixed());
+  } else if (typeof value === "string") {
+    console.log(value.toUpperCase());
+  } else if (value instanceof Date) {
+    console.log(value.getTime());
+  } else if (value && "age" in value) {
+    console.log(`${value.name}은/는 ${value.age}살 입니다.`);
+  }
+}
+
+const person: Person = {
+  name: "서니",
+  age: 20,
+};
+
+func(3);
